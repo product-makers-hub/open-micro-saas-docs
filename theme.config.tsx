8082cf529f8e0577d964b63f9d16c9a6db6a9293
Juan Correa <juan.correa.herrera@gmail.com>
2024-02-13 14:02:22 -0600

    add favicon images and update theme.config.tsx

--- theme.config.tsx
+++ theme.config.tsx
@@ -22,6 +22,41 @@ const config: DocsThemeConfig = {
       </a>
     ),
   },
+  useNextSeoProps() {
+    return {
+      additionalLinkTags: [
+        {
+          href: "favicon/apple-icon-180x180.png",
+          rel: "apple-touch-icon",
+          sizes: "180x180",
+        },
+        {
+          href: "favicon/android-icon-192x192.png",
+          rel: "icon",
+          sizes: "192x192",
+          type: "image/png",
+        },
+        {
+          href: "favicon/favicon-96x96.png",
+          rel: "icon",
+          sizes: "96x96",
+          type: "image/png",
+        },
+        {
+          href: "favicon/favicon-32x32.png",
+          rel: "icon",
+          sizes: "32x32",
+          type: "image/png",
+        },
+        {
+          href: "favicon/favicon-16x16.png",
+          rel: "icon",
+          sizes: "16x16",
+          type: "image/png",
+        },
+      ],
+    };
+  },
 };
 
 export default config;
