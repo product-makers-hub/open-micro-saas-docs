15fc820a655c1334c072b09d1ea7522afb85e12e
Juan Correa <juan.correa.herrera@gmail.com>
2024-04-07 11:34:04 -0600

    add New component and feature flags

---
+++ components/new.tsx
@@ -0,0 +1,18 @@
+export const New = () => {
+  return (
+    <span
+      style={{
+        backgroundColor: "green",
+        color: "white",
+        padding: "3px 6px",
+        borderRadius: "3px",
+        fontSize: "0.6em",
+        fontWeight: "bold",
+        fontStyle: "italic",
+        textTransform: "uppercase",
+      }}
+    >
+      New
+    </span>
+  );
+};
